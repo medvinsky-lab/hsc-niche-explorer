dda9dff680b2c388bb19ddad166432bb305a3dc7
didrikolofsson <Didrik.Olofsson@gmail.com>
2023-03-31 23:11:05 +0200

    Registered dropdown global

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
+import DropDownMenu from "./components/DropDownMenu.vue";
+
 import "./assets/style.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+
+app.component("DropDownMenu", DropDownMenu);
 
 app.mount("#app");
